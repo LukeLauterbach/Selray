296bea5904aaa863e724552a9fa579b9436ee204
laleragon@gmail.com <41rUY3S2CXTF9WrpW@7H>
2025-04-25 11:08:52 -0400

    Updated README.md

--- README.md
+++ README.md
@@ -2,6 +2,15 @@
 ## Description
 Password spraying utility, utilizing Selenium to avoid common WAF detections and allowing for flexibility with target systems.
 ## Usage
+### Examples
+> -u test@example.com -p Password1 -dl 1 -uf 'name="username"' -pf 'name="password"' --url https://example.com -s Success
+
+This is the most basic usage. A single username and password will be attempted against the login portal, with all requests coming from the origin's IP address. 
+
+> -u usernames.txt -p passwords.txt -dl 60 -uf 'name="username"' -pf 'name="password"' --url https://example.com -s Success --aws -t 10 -n 10
+
+Spray multiple users contained in a file. Attempt multiple passwords from a file, with 60 minutes between each individual password, to avoid an account lockout. Use 10 AWS provies to bypass IP-based restrictions, and ensure that no more than 10 login attempts ever come from the same IP address. 
+
 ### Requirements
 Each run of the script always requires four things:
 * `-u {USERNAME/S}` - Either a username or a file with a list of passwords. Alternatively, a list of usernames and passwords can be provided, seperated by a colon (e.g. USER:PASS)
@@ -12,9 +21,10 @@ Each run of the script always requires four things:
 * `-pf {PASSWORD FIELD IDENTIFIER}` - An HTML attribute that is unique to the password field where you want passwords to be entered. For the example below, `name=Password` would work.
     
     ```<input id="passwordInput" name="Password" type="password" tabindex="2" class="text fullWidth" placeholder="Password" autocomplete="off">```
+
 Additionally, the script needs to be told what a valid login looks like. That can be accomplished with one of the following two things:
 * `-f` - Text which only appears on the page if a failed login occurs.
-* `-s` - Text which only appears on the page if a successful login occurs. 
+* `-s` - Text which only appears on the page if a successful login occurs.
 ### Options
 The following arguments can be provided to change the execution of the script. 
 * `-t` - Change the number of concurrent login attempts (the default is 5). Keep in mind, this script is emulating a browser for each thread, so it will inherently be slower than most POST-based spraying tools.
@@ -23,6 +33,42 @@ The following arguments can be provided to change the execution of the script.
 * `-cb` - If a checkbox is required, provide a unique attribute of the checkbox, allowing the script to automatically check it. For example, if `<input type='checkbox'>`, enter 'type='checkbox''
 * `-d` - Prefix all usernames with a domain (e.g. DOMAIN\USERNAME)
 * `-da` - Postfix all usernames with a domain (e.g. USERNAME@DOMAIN)
+
+## Proxies
+Selray can optionally spin up proxies in AWS EC2. The proxies will be automatically deployed, stopped while delaying between passwords, and then destroyed when the spray completes.
+
+To use the AWS proxy functionality, use `--aws`
+  
+### Proxy Options
+* `-n` - Number of spray attempts per unique IP address. Default is 5. Every IP rotation will add a delay of approximately 1 minute on the thread. 
+* `-t` - An EC2 instance will be created for each thread. The more threads, the faster the script, but the more EC2 instances that will be created. Default is 5. Up to 29 can be created with the AWS Free Tier. 
+* `--proxy-list` - Lists all AWS EC2 instances created by Selray
+* `--proxy-clean` - Destroys all AWS EC2 instances created by Selray (will not delete other EC2 instances).
+
+### AWS Credential Configuration
+AWS credentials can be configured in one of two ways:
+* Environment Variables (Recommended)
+  * `AWS_ACCESS_KEY_ID`
+  * `AWS_SECRET_ACCESS_KEY`
+  * `AWS_DEFAULT_REGION`
+* Arguments
+  * `--aws-access-key {ACCESS KEY}` 
+  * `--aws-secret-key {SECRET ACCESS KEY}`
+  * `--aws-region {AWS REGION}` - The default is US-EAST-2
+
+### AWS Credential Setup
+1. Log into the [IAM Console](https://us-east-1.console.aws.amazon.com/iam). 
+2. Navigate to the `Users` tab, and then select `Create User`
+   1. User name doesn't matter, and the account does not need access to the AWS Management Console
+   2. `Attach Policies Directory`, and then select `AmazonEC2FullAccess`
+   3. Finish creating the user
+3. Select the user, and then select `Create access key`
+   1. Select `Other` as the use case
+   2. Copy the access key and secret access key from the final page
+
+Your region can be selected from [AWS' list of regions](https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.RegionsAndAvailabilityZones.html). 
+
+The actual access the script needs could (and should) probably be limited to more specific privileges. 
 
 ## Installation
 `pipx install "git+https://github.com/LukeLauterbach/SeleniumSpray"`
--- selray/utils/utils.py
+++ selray/utils/utils.py
@@ -11,24 +11,11 @@ def parse_arguments():
 
     required.add_argument('--url',
                           help="(REQUIRED) URL of the website to spray.")
-    optional.add_argument("-d", "--domain",
-                          help="(OPTIONAL) Prefix all usernames with a domain (e.g. DOMAIN\\USERNAME)")
-    optional.add_argument("-da", "--domain-after", action="store_true",
-                          help="(OPTIONAL) Append domain to the end of the username (e.g. username@domain)")
-    required.add_argument("-p", "--passwords",
-                          help="(REQUIRED) Password or file with list of usernames to spray.")
-    condition.add_argument('-f', '--fail',
-                           help="(OPTIONAL) Text which will be on the page if authentication fails. -s can be used as "
-                                "an alternative.")
-    condition.add_argument('-s', '--success',
-                           help="(OPTIONAL) Text which will be on the page if authentication is successful. -f can be "
-                                "used as an alternative")
     required.add_argument("-u", "--usernames",
                           help="(REQUIRED) Username or file with list of usernames to spray. Alternatively, can be a"
                                "list of colon-seperated credentials to spray (e.g. USER:PASS)")
-    optional.add_argument("-i", "--invalid-username", type=str,
-                          help="(OPTIONAL) String(s) to look for to determine if the username was invalid. Multiple "
-                               "strings can be provided comma seperated with no spaces.")
+    required.add_argument("-p", "--passwords",
+                          help="(REQUIRED) Password or file with list of usernames to spray.")
     required.add_argument('-uf', '--username-field', type=str,
                           help="(REQUIRED) Input field attribute, used to identify which field should be used to put"
                                "a username into. Can be found by inspecting the username field in your browser. For"
@@ -37,10 +24,13 @@ def parse_arguments():
                           help="(REQUIRED) Input field attribute, used to identify which field should be used to put"
                                "a username into. Can be found by inspecting the username field in your browser. For"
                                "example, if '<input type='email'>', enter 'type='email''")
-    optional.add_argument('-cb', '--checkbox', type=str,
-                          help="(OPTIONAL) If a checkbox is required, provide a unique attribute of the checkbox, "
-                               "allowing the script to automatically check it. For example, if "
-                               "'<input type='checkbox'>', enter 'type='checkbox''")
+
+    condition.add_argument('-f', '--fail',
+                           help="(OPTIONAL) Text which will be on the page if authentication fails. -s can be used as "
+                                "an alternative.")
+    condition.add_argument('-s', '--success',
+                           help="(OPTIONAL) Text which will be on the page if authentication is successful. -f can be "
+                                "used as an alternative")
 
     optional.add_argument('-t', '--threads', type=int, default=5,
                           help="(OPTIONAL) Number of threads for passwords spraying. Lower is stealthier. "
@@ -48,11 +38,22 @@ def parse_arguments():
     optional.add_argument('-dl', '--delay', type=int, default=30,
                           help="(OPTIONAL) Length of time between passwords. The delay is between the first spray "
                                "attempt with a password and the first attempt with the next password. Default is 30.")
-    optional.add_argument('-n','--num_sprays_per_ip', type=int, default=5, help="(OPTIONAL) Number of sprays to perform per IP address. Default is 5.")
-    optional.add_argument('--aws', action='store_true', help="(OPTIONAL) Use AWS proxies. Default is False.")
-    optional.add_argument('--proxy-clean', action='store_true', help="(OPTIONAL) Clean up all created proxies, instead of spraying.")
-    optional.add_argument('--proxy-list', action='store_true', help="(OPTIONAL) List all created proxies.")
+    optional.add_argument("-d", "--domain",
+                          help="(OPTIONAL) Prefix all usernames with a domain (e.g. DOMAIN\\USERNAME)")
+    optional.add_argument("-da", "--domain-after", action="store_true",
+                          help="(OPTIONAL) Append domain to the end of the username (e.g. username@domain)")
+    optional.add_argument("-i", "--invalid-username", type=str,
+                          help="(OPTIONAL) String(s) to look for to determine if the username was invalid. Multiple "
+                               "strings can be provided comma seperated with no spaces.")
+    optional.add_argument('-cb', '--checkbox', type=str,
+                          help="(OPTIONAL) If a checkbox is required, provide a unique attribute of the checkbox, "
+                               "allowing the script to automatically check it. For example, if "
+                               "'<input type='checkbox'>', enter 'type='checkbox''")
 
+    aws_group.add_argument('--aws', action='store_true', help="(OPTIONAL) Use AWS proxies. Default is False.")
+    aws_group.add_argument('-n','--num_sprays_per_ip', type=int, default=5, help="(OPTIONAL) Number of sprays to perform per IP address. Default is 5.")
+    aws_group.add_argument('--proxy-clean', action='store_true', help="(OPTIONAL) Clean up all created proxies, instead of spraying.")
+    aws_group.add_argument('--proxy-list', action='store_true', help="(OPTIONAL) List all created proxies.")
     aws_group.add_argument("--aws-access-key", help="AWS Access Key ID")
     aws_group.add_argument("--aws-secret-key", help="AWS Secret Access Key")
     aws_group.add_argument("--aws-session-token", help="AWS Session Token (optional)")
