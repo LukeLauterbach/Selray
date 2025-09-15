d7180c051da51570f5028129432613132499332a
Luke Lauterbach <104774644+LukeLauterbach@users.noreply.github.com>
2025-09-15 09:38:16 -0400

    Update Selray.py

--- selray/Selray.py
+++ selray/Selray.py
@@ -15,7 +15,6 @@ __version__ = "0.7"
 def main():
     utils.initialize_playwright()
     args = utils.parse_arguments()
-    results = []
 
     # Prepare spray configuration
     args = prepare_variables.main(args)
